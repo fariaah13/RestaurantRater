64fa2f6097b4563477bc884c95c9c82414f96564
fariaah13 <fariaah13@gmail.com>
2019-08-22 20:19:45 -0400

    initial commit

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+
+        //database table will have these fields
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        //This is what we update and add to and save
+        //This is our table so the name can be plural
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
